76a3c5f9ab692b55097a3a1029328f984c8db4cb
wocheon <wocheon07@gmail.com>
2025-04-23 15:56:17 +0900

    202504231556

--- modules/aws_s3_access_point/main.tf
+++ modules/aws_s3_access_point/main.tf
@@ -2,6 +2,6 @@ resource "aws_s3_access_point" "this" {
   name       = var.aws_s3_access_point_name  
   bucket     = var.aws_s3_bucket  
   policy     = var.aws_s3_access_point_policy
-  vpc_id     = var.aws_s3_access_point_vpc_id
-  tags       = var.aws_s3_access_point_tags
+  #vpc_id     = var.aws_s3_access_point_vpc_id
+  #tags       = var.aws_s3_access_point_tags
 }
--- modules/aws_s3_account_public_access_block/main.tf
+++ modules/aws_s3_account_public_access_block/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_account_public_access_block" "this" {
-  bucket                       = var.aws_s3_bucket
+  #bucket                       = var.aws_s3_bucket
   block_public_acls            = var.aws_s3_apab_block_public_acls
   block_public_policy          = var.aws_s3_apab_block_public_policy
   ignore_public_acls           = var.aws_s3_apab_ignore_public_acls
