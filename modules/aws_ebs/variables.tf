17a93412d3f51a72eb30a1b386d24ff4aa72e970
wocheon <wocheon07@gmail.com>
2025-04-23 09:39:46 +0900

    202504230939

--- modules/aws_ebs/variables.tf
+++ modules/aws_ebs/variables.tf
@@ -1,44 +1,51 @@
-variable "availability_zone" {
+variable "ebs_availability_zone" {
   description = "The AZ where the EBS volume will be created"
   type        = string
 }
 
-variable "size" {
+variable "ebs_size" {
   description = "The size of the drive in GiBs"
   type        = number
 }
 
-variable "type" {
+variable "ebs_type" {
   description = "The type of EBS volume"
   type        = string
   default     = "gp3"
 }
 
-variable "iops" {
+variable "ebs_iops" {
   description = "The amount of provisioned IOPS"
   type        = number
   default     = null
 }
 
-variable "throughput" {
+variable "ebs_throughput" {
   description = "The throughput to provision in MiB/s (only for gp3)"
   type        = number
   default     = null
 }
 
-variable "encrypted" {
+variable "ebs_encrypted" {
   description = "Whether the volume should be encrypted"
   type        = bool
   default     = true
 }
 
-variable "kms_key_id" {
+variable "ebs_kms_key_id" {
   description = "The full ARN of the KMS key to use when encrypting the volume"
   type        = string
   default     = null
 }
 
-variable "tags" {
+variable "ebs_source_snapshot" {
+  description = "The Source Snapshot ID of volume"
+  type        = string
+  default     = null
+}
+
+
+variable "ebs_tags" {
   description = "Tags to assign to the volume"
   type        = map(string)
   default     = {}
