a38ef2ed6b8cd1773af9bd084de0141d19dc6dee
wocheon <wocheon07@gmail.com>
2025-04-23 11:05:57 +0900

    202504231105

--- modules/aws_ebs_snapshot/main.tf
+++ modules/aws_ebs_snapshot/main.tf
@@ -5,4 +5,7 @@ resource "aws_ebs_snapshot" "this" {
   permanent_restore       = var.ebs_snapshot_permanent_restore
   temporary_restore_days  = var.ebs_snapshot_temporary_restore_days
   tags                    = var.ebs_snapshot_tags
+  timeouts {
+    create = "30m"  # 30분 동안 기다리도록 설정
+  }
 }
